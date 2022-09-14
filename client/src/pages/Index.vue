c0a6a5acc4e6f05d3be8eb6e154f77bf3fcb6d78
Archana Prabhu <garchanaprabhu@gmail.com>
2022-09-14 20:36:17 +0530

    Disclaimer on responsibility of the track and trace only.
    Signed-off-by: Archana Prabhu <garchanaprabhu@gmail.com>

--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-  <p class="text-faded">All the actions listed on the left panel are for demo purpose only.Only View History of the Batch(Track and Trace) is the primary responsiblity of the app here</p>
+  <p class="text-faded">All the actions listed on the left panel are for simulation , demo purpose only.View History of the Batch(Track and Trace) is the only responsibility of the app here</p>
     <img alt="Quasar logo" src="~assets/BY.png">
   </q-page>
 
